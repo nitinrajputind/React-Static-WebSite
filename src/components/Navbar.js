5a1990088f6812cf312c6b1520ada2d52cdf160d
nitinrajputind <nitinrajput971625@gmail.com>
2023-07-20 14:54:58 +0530

    NavBar is Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import "./navbar.css";
 import {FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
+  const[showMediaIcon, setshowMediaIcon] = useState(false);
+  // console.log(showMediaIcon)
+
+
   return (
     <div>
       <nav className='main_nav'>
@@ -14,7 +19,7 @@ const Navbar = () => {
         </div>
 
         {/* second menu part  */}
-        <div className="menu_link">
+        <div className= {showMediaIcon ? "menu_link mobile-menu-link" : "menu_link"}>
             <ul>
                 <li><NavLink  className={({ isActive }) => (isActive ? 'active_class' : 'notactive_class')} to={"/"} >Home</NavLink></li>
                 <li><NavLink  className={({ isActive }) => (isActive ? 'active_class' : 'notactive_class')} to={"/about"}>about</NavLink></li>
@@ -25,11 +30,16 @@ const Navbar = () => {
 
         {/* third social Link */}
         <div className="social_media">
-             <ul className='social-media-desktop'>
+             <ul className='social_media_desktop'>
                 <li><a href="https://www.facebook.com/nitinrajputindia/" target='/blank' ><FaFacebook className='facebook'/></a></li>
                 <li><a href="https://www.instagram.com/nitinrajputindia/" target='/blank' ><FaInstagram className='instagram'/></a></li>
                 <li><a href="https://github.com/nitinrajputind" target='/blank' ><FaGithub className='github' /></a></li>
              </ul>
+
+             {/* hamburger menu start */}
+             <div className="hamburger_menu">
+              <Link  onClick={()=> setshowMediaIcon(!showMediaIcon)} ><GiHamburgerMenu/></Link>
+             </div>
         </div>
 
       </nav>
